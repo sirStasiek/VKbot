241796f95bff5187a14084519b930a3355333dbc
sirStasiek <akinin-stanislav@mail.ru>
2023-06-15 17:02:52 +0300

    vkbot
    vkbot

---
+++ Бот для чата ВК/config.py
@@ -0,0 +1,5 @@
+comunity_token = 'vk1.a.ey_DI99U48IIUqjr8WzAOuSY_m1dlcUBir9-p4LoNHl9dOiNybpzsgrA'
+acces_token = 'vk1.a.76SewXFQ1MkBUvVCWDaow_jOv_ZPujwofJyWteWmINMvoI-qTvwnc6_Yl8DJ'
+
+
+db_url_object = "postgresql://postgres:1234@localhost:5432/student_diplom"
